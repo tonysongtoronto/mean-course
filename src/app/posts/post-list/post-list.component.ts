ab4e265f2ead2d6a226692edbdd63f8bea9b8511
tony <songjf@yahoo.com>
2025-09-06 17:26:57 -0400

    section 4

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -37,6 +37,11 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   }
 
+    onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
+
+
   ngOnDestroy() {
     this.postsSub.unsubscribe();
   }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Subject } from "rxjs";
+import { map, Subject } from "rxjs";
 
 import { Post } from "./post.model";
 
@@ -20,13 +20,22 @@ export class PostsService {
       return result;
     }
 
-  getPosts() {
+getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
+      .get<{ message: string; posts: any[] }>(
         "http://localhost:3000/api/posts"
       )
-      .subscribe(postData => {
-        this.posts = postData.posts;
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe(transformedPosts => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -37,12 +46,23 @@ export class PostsService {
 
 
 addPost(title: string, content: string) {
-    const post: Post = { id: this.generateRandomString(10), title: title, content: content };
+    const post: Post = { id: null, title: title, content: content };
     this.http
-      .post<{ message: string }>("http://localhost:3000/api/posts", post)
+      .post<{ message: string, postId:string }>("http://localhost:3000/api/posts", post)
       .subscribe(responseData => {
         console.log(responseData.message);
+        console.log(responseData.message);
+        post.id=responseData.postId
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+    deletePost(postId: string) {
+    this.http.delete("http://localhost:3000/api/posts/" + postId)
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
